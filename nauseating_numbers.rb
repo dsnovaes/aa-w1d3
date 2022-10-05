5d55c9ea9c9ff761ef3872bc0b7e616864c481f2
Diego Novaes <dsnovaes1@gmail.com>
2022-10-05 13:55:04 -0300

    finished nauseating_numbers

--- nauseating_numbers.rb
+++ nauseating_numbers.rb
@@ -34,3 +34,20 @@ end
 # p pair_product([4, 2, 5, 7], 16)    # false
 # p pair_product([8, 4, 9, 3], 8)     # false
 # p pair_product([3], 12)             # false
+
+def rampant_repeats(str, hash)
+    new_str = ""
+    str.each_char do |char|
+        if hash.has_key?(char)
+            new_str += char * hash[char]
+        else
+            new_str += char
+        end
+    end
+    new_str
+end
+
+# p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+# p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+# p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+# p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
